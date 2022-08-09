9a9f8c26def2937d4667e510a903dd7af97574a8
Kirill Drozdov <kirilldrozd12@gmail.com>
2022-08-09 23:53:23 +0300

    нашел сыль на фигму

--- EasySwift-UIKit/AppDelegate.swift
+++ EasySwift-UIKit/AppDelegate.swift
@@ -11,7 +11,7 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 //TODO: - ДОПИЛИТЬ ПРИЛУ
-    // - ДИЗАЙН ВЗЯТЬ ИЗ ФИГМЫ
+    // - ДИЗАЙН ВЗЯТЬ ИЗ ФИГМЫ (https://www.figma.com/file/Rj7pECH4t2868iJjvi5Irw/Untitled?node-id=0%3A1)
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
