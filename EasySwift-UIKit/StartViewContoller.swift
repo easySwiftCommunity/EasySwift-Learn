7ca772c5aa28c73ab499f8f16f36a41479eb9b25
Kirill Drozdov <kirilldrozd12@gmail.com>
2022-08-11 00:10:22 +0300

    Я всё удалил. Если кто будет брать изменения, то это ветку пока что не трогайте,  ВОООБЩЕ

---
+++ EasySwift-UIKit/StartViewContoller.swift
@@ -0,0 +1,17 @@
+//
+//  StartViewContoller.swift
+//  EasySwift-UIKit
+//
+//  Created by Kirill Drozdov on 11.08.2022.
+//
+
+import Foundation
+import UIKit
+
+class StartViewContoller: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .red
+    }
+}
